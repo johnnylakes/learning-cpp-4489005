60afeeea23ea403f8deff0f8d17ff0de8663777c
João lago <66376062+johnnylakes@users.noreply.github.com>
2024-05-16 16:42:28 +0000

    exercise solutions

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,9 +7,10 @@
 
 int main(){
     std::string str;
+    std::cout << "Enter your name:" << std::flush;
     std::cin >> str;
-    std::cout << str;
-
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;
+ 
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+int a, b = 2; // single line comment
+/*multi-line comment*/
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 4;
+    my_flag = false;
+    std::cout << "a =" << a << std::endl;
+    std::cout << "b =" << b << std::endl;
+    std::cout << "my_flag ="  << my_flag << std::endl;
+    std::cout << "a + b ="  << a + b << std::endl;
+    std::cout << "a - b ="  << a - b << std::endl;
+    std::cout << "a / b ="  << a / b << std::endl;
+    std::cout << "a * b ="  << a * b << std::endl;
+    signed int positive;
+    positive = - a - b;
+    std::cout << "positive = " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
